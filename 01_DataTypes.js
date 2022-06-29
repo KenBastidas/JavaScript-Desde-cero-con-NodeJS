6b3f5974afa63f54ed076828f25cd5b88bd5ea9f
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2022-06-28 22:44:25 -0500

    Hello World and DataTypes

---
+++ 01_DataTypes.js
@@ -0,0 +1,35 @@
+//This is a line comment
+/**
+ * Data Types
+ */
+//Number - Numero
+console.log(27);
+console.log(27.56);
+//Text - String -Texto
+console.log("Garfield Aguero");
+console.log("Giorno Giovanna");
+console.log(`Hi, I am Ken`);
+//Booleans - Logicos
+console.log(true);
+console.log(false);
+//Undefined - Indefinido
+console.log(undefined);
+//null
+console.log(null);
+/**
+ * Stuctures DataTypes
+ */
+//Object - Objeto
+console.log({ name: "Garfield", age: 24, DatBirth: null });
+console.log({ name: "Garfield", age: 24, DatBirth: "25/08/2002" });
+console.log({
+  name: "Garfield",
+  age: 24,
+  DatBirth: "25/08/2002",
+  isTall: false,
+  cities: ["Quito", "Guayaquil", "Buenos Aires"],
+  address: { Principal: "Siempre Viva" },
+});
+//Data Type from List Type - array
+console.log(["Mexico", "Venezuela", "Ecuador"]);
+console.log([21, true, { name: "GARFIELD" }, [1, 2, 3, 4, 54]]);
